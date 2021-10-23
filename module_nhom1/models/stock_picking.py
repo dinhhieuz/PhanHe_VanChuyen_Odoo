8e8767e16fe1b95f10ea4272db517380d1f9e498
dinhhieuz <78039925+dinhhieuz@users.noreply.github.com>
2021-10-23 08:55:24 +0700

    hi

--- module_nhom1/models/stock_picking.py
+++ module_nhom1/models/stock_picking.py
@@ -1,6 +1,28 @@
 from odoo import api, fields, models
+
 class stock(models.Model):
     _inherit = "stock.picking"
     _description = "Module giao hàng nhóm 1 kế thừa từ odoo"
     # ahieu=fields.Text(string='Anh Hiếu')
+    tinhtrang_phieu = fields.Selection([
+        ('wait', 'Chờ chuyển'),
+        ('doing', 'Đang chuyển'),
+        ('done', 'Đã chuyển'),
+        ('delay', 'Trì hoãn chuyển')
+    ], string='Tình trạng phiếu', default='wait')
+    # sử dụng vòng lập foreach để lấy danh sách vào trong Selection (làm sau)
+    nguoitao_phieu = fields.Char('Pet Name', required=True)
+    ngaytao_phieu = fields.date.today()
+
+    IDnguoitao_phieu = fields.One2many(comodel_name='res.users', inverse_name='rel_id')
+
+    # describing customer
+    # tự động sinh mã khi tạo (làm sau)
+
+    id_khachhang = fields.One2many(comodel_name='res.users', inverse_name='rel_id')
+
+
+
+
+
 
