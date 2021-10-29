de65d8c43f335f4b90444fbffccb6f8222497a7b
dinhhieuz <78039925+dinhhieuz@users.noreply.github.com>
2021-10-29 20:33:48 +0700

    update v2

---
+++ module_nhom1/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import shipper_nhom1
+from . import stock_picking
+from . import phuongtien_nhom1
---
+++ module_nhom1/models/shipper_nhom1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from odoo import _, api, fields, models
+class nhom1(models.Model):
+    _name = "shipper.nhom1"
+    _description = "Module vận chuyển nhóm 1"
+
+    name = fields.Char(string='Mã SP', required=True, copy=False, readonly=True,
+                       default=lambda seft: _('New'))
+    ten = fields.Char(string='Tên Shipper', required = True)
+    avatar = fields.Binary("Avatar")
+    ngaysinh = fields.Date("Ngày sinh")
+    diachi = fields.Text("Địa chỉ nhà")
+    sdt = fields.Char("Số điện thoại")
+    cmnd = fields.Char("CCMND")
+    mota = fields.Text('Mô tả công việc')
+    covid = fields.Boolean('Đã tiêm Covid', default=False)
+    gioitinh = fields.Selection([('nam', 'Nam'), ('nu', 'Nữ')], string='Giới tính', default='nam')
+    trangthai = fields.Selection([('danggiaohang', 'Đang giao hàng'), ('chogiaohang', 'Chờ đơn hàng')], string='Trạng thái', default='')
+    nameprivate = fields.Char('Tên công việc')
+    image = fields.Binary("Hình ảnh đơn hàng")
+
+    _sql_constraints = [
+        ('cmnd', 'unique (cmnd)', 'CMND đã tồn tại, vui lòng nhập khác!!!')
+    ]
+
+
+    # def custom_remove(self):
+    #         for module in self:
+    #             module.unlink()
+    #         pass
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', ('New')) == ('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('shipper.nhom1') or _('New')
+        res = super(nhom1, self).create(vals)
+        return res
