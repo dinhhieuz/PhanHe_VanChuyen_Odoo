a89d7ef1f319ca1d65337eae0840c2e1ff71c2c4
dinhhieuz <78039925+dinhhieuz@users.noreply.github.com>
2021-10-29 20:35:18 +0700

    del v2

--- by_nhom1/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import stock_picking
-from . import shipper_nhom1
-from . import phuongtien_nhom1
--- module_nhom1/models/nhanvien.py
+++
@@ -1,4 +0,0 @@
-from odoo import _, api, fields, models
-class nhanvien(models.Model):
-    _inherit = 'hr.employee'
-    _description = "Prototype inheritance_kế thừa từ model nhân viên trong odoo"
